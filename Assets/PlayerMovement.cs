cdb437d9168399cbc560b5b72ca7926138b32e7c
Kamil Kwiatkowski <kamilkwiatkos@gmail.com>
2024-01-03 02:29:52 +0100

    ToCoBędzieJutro
    :)

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float MoveSpeed;
+    public float XMouseSpeed;
+    public float YMouseSpeed;
+    public GameObject camera;
+    public GameObject bullet;
     Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
@@ -25,24 +31,39 @@ public class PlayerMovement : MonoBehaviour
         //Modyfikujemy kierunek poruszania siê w zale¿noœci od wciœniêcia przcisków
         if(forward)
         {
-            moveDirection.z = 1;
+            moveDirection += transform.forward;
         }
         if(back)
         {
-            moveDirection.z = -1;
+            moveDirection += -transform.forward;
         }
         if (left)
         {
-            moveDirection.x = -1;
+            moveDirection += -transform.right;
 
         }
         if (right)
         {
-            moveDirection.x = 1;
+            moveDirection += transform.right;
         }
 
         //Dodajemy si³ê do rigidbody by poruszyæ gracza w odpowienim kierunku
         //Mno¿ymy moveDirection przez MoveSpeed by umo¿liwiæ kontrolê prêdkoœci
-        rb.AddForce(moveDirection * MoveSpeed);
+        //rb.AddForce(moveDirection * MoveSpeed);
+        rb.velocity = new Vector3(moveDirection.x * MoveSpeed, rb.velocity.y, moveDirection.z * MoveSpeed);
+
+        var x = Input.GetAxis("Mouse X") * XMouseSpeed;
+        var y = -Input.GetAxis("Mouse Y") * YMouseSpeed;
+
+        camera.transform.Rotate(Vector3.right, y);
+        transform.Rotate(Vector3.up, x);
+
+
+        bool mousePressed = Input.GetMouseButton(0);
+        if (mousePressed)
+        {
+            var shoot = Instantiate(bullet, camera.transform.position, camera.transform.rotation);
+            shoot.GetComponent<Rigidbody>().velocity = camera.transform.forward * 100;
+        }
     }
 }
